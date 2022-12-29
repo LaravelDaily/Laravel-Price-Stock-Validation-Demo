a3bb04d1bc50fa627898e636776e7651e472c34c
PovilasKorop <povilask007@gmail.com>
2022-12-29 11:07:12 +0200

    Check stock

--- app/Rules/ProductStockPriceRule.php
+++ app/Rules/ProductStockPriceRule.php
@@ -2,6 +2,7 @@
 
 namespace App\Rules;
 
+use App\Models\Product;
 use Illuminate\Contracts\Validation\InvokableRule;
 
 class ProductStockPriceRule implements InvokableRule
@@ -19,6 +20,22 @@ class ProductStockPriceRule implements InvokableRule
         $products = array_filter($value);
         if (count($products) == 0) {
             $fail('Please select at least one product');
+        }
+
+        $DBProducts = Product::find(array_keys($products))->keyBy('id');
+
+        $errorText = '';
+        foreach ($products as $productID => $quantity) {
+            // Check stock
+            if ($DBProducts[$productID]->stock_left < $quantity) {
+                $errorText .= 'Sorry, we have only ' . $DBProducts[$productID]->stock_left . ' of ' . $DBProducts[$productID]->name . ' left in stock. ';
+            }
+
+            // Check price
+        }
+
+        if ($errorText != '') {
+            $fail($errorText);
         }
     }
 }
